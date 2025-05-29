4f7a1424f3157da027fc7bbd95114218b241efd5
sunbowch <sunbow@bluewin.ch>
2025-05-29 14:44:10 +0200

    Update chamber_fan.py

--- klippy/extras/chamber_fan.py
+++ klippy/extras/chamber_fan.py
@@ -20,6 +20,9 @@ class ChamberFan:
         self.fan_on = True
         self.last_on = self.idle_timeout
         self.last_speed = 0.
+        self.gcode.register_command("TOGGLE_CHAMBER_FAN", self.cmd_toggle_chamber_fan)
+        self.gcode.register_command("ENABLE_CHAMBER_FAN", self.cmd_enable_chamber_fan)
+        self.gcode.register_command("DISABLE_CHAMBER_FAN", self.cmd_disable_chamber_fan)
     def handle_connect(self):
         # Heater lookup
         pheaters = self.printer.lookup_object('heaters')
